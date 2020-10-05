8122d1bc317d9a42a31e6f7fd46a41244921b24d
Chris Maxwell <chmaxwel@icloud.com>
2020-10-05 12:53:06 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -53,9 +53,37 @@ self.addEventListener('activate', function(evt) {
 
 // fetch service worker
 self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
+            })
+            .catch(err => console.log(err))
+        );
+        return;
+    }
+
     evt.respondWith(
-        caches.match(evt.request).then(function(request) {
-            return request || fetch(evt.request);
+        fetch(evt.request).catch(function() {
+            return caches.match(evt.request).then(function(response) {
+                if (response) {
+                    return response;
+                }
+                if (evt.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('/');
+                }
+            });
         })
-    )
-})
+    );
+});
