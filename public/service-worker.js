fa379d1973855c89f618d04bf948c81d5e0d32eb
Chris Maxwell <chmaxwel@icloud.com>
2020-10-05 13:00:47 +0200

    _updated fetch to `/api/transaction/bulk`

--- public/service-worker.js
+++ public/service-worker.js
@@ -56,7 +56,7 @@ self.addEventListener('fetch', function(evt) {
     if (evt.request.url.includes('/api/')) {
         evt.respondWith(
             caches
-            .open(DATA_CACHE_NAME)
+            .open(CACHE_NAME)
             .then(cache => {
                 return fetch(evt.request)
                     .then(response => {
