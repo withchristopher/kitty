0a8e560a16a8502e977f0166dea62cde986511d0
Chris Maxwell <chmaxwel@icloud.com>
2020-10-05 10:55:11 +0200

    ++ service worker

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+console.log('You have connected your server-worker.js successfully!');
+
+// install service worker
+self.addEventListener('install', function(evt) {
+    // logic
+})
+
+// activate service worker
+self.addEventListener('activate', function(evt) {
+    // logic
+})
+
+// fetch service worker
+self.addEventListener('fetch', function(evt) {
+    // logic
+})
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kitty";
 
 const app = express();
 
@@ -17,13 +17,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
