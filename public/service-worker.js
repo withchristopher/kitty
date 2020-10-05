4d88c1b55c10921c456ed2798b6545cb7b715d78
Chris Maxwell <chmaxwel@icloud.com>
2020-10-05 12:02:31 +0200

    ++ manifest.json

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,61 @@
-console.log('You have connected your server-worker.js successfully!');
+// setup service worker const variables
+const APP_PREFIX = 'Kitty';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
+    "./js/index.js",
+    "./manifest.json"
+]
 
 // install service worker
 self.addEventListener('install', function(evt) {
-    // logic
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('installing cahce: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 })
 
 // activate service worker
 self.addEventListener('activate', function(evt) {
-    // logic
-})
+    evt.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeepList = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
 
 // fetch service worker
 self.addEventListener('fetch', function(evt) {
-    // logic
+    evt.respondWith(
+        caches.match(evt.request).then(function(request) {
+            if (request) {
+                console.log('responding with cache : ' + evt.request.url)
+                return request || fetch(evt.request);
+            }
+        }))
 })
